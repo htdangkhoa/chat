40f2429e9943a791e1fd7aa57c20f84d64805793
htdangkhoa <huynhtran.dangkhoa@gmail.com>
2017-04-16 14:01:06 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var modules = require("./modules/modules"),
     server = modules.server;
 
 // Start server on port 8080.
-server.listen(8080, "0.0.0.0", function() {
+server.listen(8080, "127.0.0.1", function() {
   console.log("Connected.");
 });
